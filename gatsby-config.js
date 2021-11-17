85c4938a573249356771299e1f6cef4bbdad34ec
extracold1209 <extracold1209@gmail.com>
2021-11-17 23:59:53 +0900

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,64 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
 module.exports = {
-  siteMetadata: {
-    siteUrl: 'https://www.yourdomain.tld',
-    title: 'My Blog',
-  },
+  /* Your site config here */
+  siteMetadata: require('./site-meta-data.json'),
   plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'markdown-pages',
+        path: `${__dirname}/_data`,
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [{
+          resolve: 'gatsby-remark-prismjs',
+          options: {
+            classPrefix: 'language-',
+            inlineCodeMarker: null,
+            aliases: {},
+            showLineNumbers: false,
+            noInlineHighlight: false,
+          },
+        },
+        {
+          resolve: 'gatsby-remark-emojis',
+        }],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        // The property ID; the tracking code won't be generated without it. replace with yours
+        trackingId: 'UA-164743872-1',
+        head: true,
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Delog GatbsyJS Starter',
+        short_name: 'Delog',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#381696',
+        display: 'standalone',
+        icon: 'src/images/icon.png',
+      },
+    },
     'gatsby-plugin-sass',
-    'gatsby-plugin-image',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-mdx',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: './src/images/',
-      },
-      __key: 'images',
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'pages',
-        path: './src/pages/',
-      },
-      __key: 'pages',
-    },
-    {
-      resolve: 'gatsby-plugin-typescript',
-      options: {
-        isTSX: true, // defaults to false
-        jsxPragma: 'jsx', // defaults to "React"
-        allExtensions: true, // defaults to false
-      },
-    },
+    'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-dark-mode',
+    // siteURL is a must for sitemap generation
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-offline',
   ],
 };
